565a061204632837a8f7aea767c7401fd3ba8003
Mohammed Ben Zehaf <mben-zeh@e2r1p10.1337.ma>
2023-12-12 08:13:18 +0100

    ex04 done

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,47 +6,22 @@
 /*   By: mben-zeh <mben-zeh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 05:34:03 by mben-zeh          #+#    #+#             */
-/*   Updated: 2023/12/11 07:16:01 by mben-zeh         ###   ########.fr       */
+/*   Updated: 2023/12/12 06:25:49 by mben-zeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sed.hpp"
 #include <iostream>
-void Replace(std::fstream &file_in, std::fstream &file_out)
-{
-    std ::string line;
-   
-    if (!file_out)
-    {
-        file_in.close();
-        return;
-    }
-    
-    while (std::getline(file_in,line))
-    {
-        file_out << line;
-        std :: cout << "oky" ;
-    }
-     
-}
+
 int main(int ac, char **av)
 {
     if (ac == 4)
     {
-        std ::fstream file_in;
-        std ::fstream file_out;
-        std::string file = std::string(av[1]);
-        file_in.open(file, std::ios::in);
-        if (file_in)
-        {
-            file_out.open(file.append(".replace"), std::ios::out|std::ios::app);
-            Replace(file_in,file_out);
-        }
-        if (!file_in || !file_out)
-        {
-            std ::cerr << BOLDRED << file << " Error: Unable to access file;" << std ::endl;
-            exit(1);
-        }
+        std::string FileName(av[1]);
+        std::string S1(av[2]);
+        std::string S2(av[3]);
+
+        sed(FileName,S1,S2);
     }
     else
     {
@@ -55,6 +30,15 @@ int main(int ac, char **av)
     }
     return (0);
 }
+
+// int main ()
+// {
+//   std::string str ("There are two needlesin this haystack with needles.");
+//   std::string str2 ("needle");
+//   std::cout << sed(str,str2);
+//   return (0);
+// }
+
 // std :: cerr << "Error: Failed to read from file: " << filename << std::endl;
 // "Error: Operation failed."
 // Error: Something went wrong.
--- CPP01/ex04/sed.cpp
+++ CPP01/ex04/sed.cpp
@@ -6,8 +6,61 @@
 /*   By: mben-zeh <mben-zeh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 06:38:29 by mben-zeh          #+#    #+#             */
-/*   Updated: 2023/12/11 06:40:23 by mben-zeh         ###   ########.fr       */
+/*   Updated: 2023/12/12 08:09:36 by mben-zeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sed.hpp"
+
+// std::string Repalce(std::string &line , std::string &to_find, std::string &replace)
+// {
+//     std::string NewLine;
+//     size_t pos_begin;
+//     size_t pos_end;
+//     size_t len ;
+
+//     len = to_find.length();
+//     pos_begin = 0;
+//     while ((pos_end = line.find(to_find,pos_begin)) != std::string::npos)
+//     {
+//         NewLine.append(line.substr(pos_begin,pos_end - pos_begin).append(replace));
+//         pos_begin = pos_end + len;
+//     }
+//     if(pos_begin < line.length())
+//         NewLine.append(line.substr(pos_begin,line.length() - pos_begin));
+//     return (NewLine);
+// }
+void sed(std::string FileName, std::string &S1, std::string &S2)
+{
+    std::fstream FileIn;
+    std::string _FileName;
+    std::string line;
+    size_t pos_start;
+    size_t pos_end ;
+
+    _FileName = std::string(FileName).append(".replace");
+    FileIn.open(FileName, std::ios::in);
+    if (FileIn)
+    {
+        std::fstream FileOut(_FileName, std::ios::out);
+        if (FileOut)
+        {
+            while (std::istream &getl = std::getline(FileIn,line))
+                {
+                    for(pos_start = 0;(pos_end = line.find(S1,pos_start)) != std::string::npos ; pos_start = pos_end + S1.length())
+                    {
+                        FileOut << line.substr(pos_start,pos_end - pos_start).append(S2);
+                    }
+                    if( pos_start < line.length())
+                        FileOut << line.substr(pos_start,line.length() - pos_start);
+                    if(!getl.eof())
+                        FileOut << std::endl;
+                }
+            FileIn.close();
+            FileOut.close();
+            return ;
+        }
+    }
+    std ::cerr << BOLDRED << ((!FileIn) ? FileName : (FileIn.close(), _FileName)) << " Error: Unable to access file;" << std ::endl;
+    exit(1);
+}
